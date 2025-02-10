a0a3ec4474aa6569be3f5744ff40f7543d27600b
Ximo-cm <iesvda.joamer@gmail.com>
2025-02-10 10:16:50 +0000

    Add Supabase integration

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('fetchData').addEventListener('click', async () => {
             'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3eWtpZ2F5aWt1bnFtcXpyYXFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5MTk0NjUsImV4cCI6MjA1NDQ5NTQ2NX0.9uQb77rpaZNOLvBdttL5Xhky04Wb2Xzw3xnD4fNz02c',
             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3eWtpZ2F5aWt1bnFtcXpyYXFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5MTk0NjUsImV4cCI6MjA1NDQ5NTQ2NX0.9uQb77rpaZNOLvBdttL5Xhky04Wb2Xzw3xnD4fNz02c'
 	    'Content-Type': 'application/json'  
-  }
+  },
 	mode:"cors"
     });
 
